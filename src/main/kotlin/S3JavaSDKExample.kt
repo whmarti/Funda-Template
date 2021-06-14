a9ee758711dfc89101741edee0e248665fe66c3c
Dev. William Martin <whmarti@hotmail.com>
2021-06-14 19:40:09 +1200

    Ajustes con nueva version aws-jdk-S3

--- src/main/kotlin/S3JavaSDKExample.kt
+++ src/main/kotlin/S3JavaSDKExample.kt
@@ -1,8 +1,16 @@
-import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.PutObjectRequest
-import com.amazonaws.services.s3.model.PutObjectResult
+//import com.amazonaws.auth.BasicAWSCredentials
+//import com.amazonaws.services.s3.AmazonS3Client
+//import com.amazonaws.services.s3.model.PutObjectRequest
+//import com.amazonaws.services.s3.model.PutObjectResult
+
+
 import org.apache.commons.io.FileUtils
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.core.sync.ResponseTransformer
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.*
 import utils.Credentials
 import java.io.File
 import java.io.FileOutputStream
@@ -10,6 +18,8 @@ import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
+
 
 /**
  * Created by William Martin on 27/05/2021.
@@ -20,10 +30,9 @@ object S3JavaSDKExample : Throwable() {
     fun main(args: Array<String>) {
         println("Get in...")
         //BringFiles(Credentials.FILE_TO_COPY)
-       // Prueba()
-                PopulateSimpleBucket()
-
-      //  PopulateBucket(donations)
+        // Prueba()
+//        PopulateSimpleBucket()
+        //  PopulateBucket(donations)
 
 //        val donations = listOf(donationLink("8uutxhfsTseQnUetuWax","https://drive.google.com/uc?export=download&id=1w_yIZSDbFFfQ5fPYfhaX_oHs_kBpWkMn"),
 //            donationLink("56OpQxFvd2SuPlJ1xnwP","https://drive.google.com/uc?export=download&id=1lwN4M7QDcXtpIdMLlrAUhyjszqmwD44H"),
@@ -31,24 +40,79 @@ object S3JavaSDKExample : Throwable() {
 //            donationLink("DIl3neHSd0BhA3kaPjBf","https://drive.google.com/uc?export=download&id=1f3YbJv5_c2582-qUcdttJXHyZoVr_3MQ"),
 //            donationLink("F9i6rzAz6DC43c3UYVZW","https://drive.google.com/uc?export=download&id=1sKXbLuuF4ILOj-DHWxNipu3QJCW1DHEx") )
 //        DBloadFiles(donations)
+//        ListDocumentsAWS()
+        PopulateSimpleAWSBucket()
+    }
+
+    private fun PopulateSimpleAWSBucket() {
+        val bucketName = "donaciones"
+        val fileName = Credentials.FILE_TO_COPY
+        val key_name = Credentials.PATH_FILE + fileName
+        val file = File(Credentials.PATH_FILE + fileName)
+        val awsCreds = AwsBasicCredentials.create(
+            Credentials.ACCESS_KEY_ID,
+            Credentials.SECRET_ACCESS_KEY
+        )
+        val region: Region = Region.US_EAST_1
+        val s3 = S3Client.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+            .region(region)
+            .build()
+        try {
+            val key = UUID.randomUUID().toString()
+            val request: PutObjectRequest = PutObjectRequest.builder()
+                .key(fileName)
+                .bucket(bucketName)
+                .build()
+            s3.putObject(request, file.toPath())
+
+            //Solo por verificacion:
+            val getObjectRequest = GetObjectRequest.builder()
+                .key(fileName)
+                .bucket(bucketName)
+                .build()
+            val response: String = s3.getObject(getObjectRequest, ResponseTransformer.toBytes()).asUtf8String()
+            val response1 = s3.getObject(getObjectRequest)
+//            System.out.println("Uploaded object encryption status is " +response )
+//            System.out.println("Uploaded object encryption status is " +response1 )
+            //Fin verificacion.
+            System.out.println("File (s) Uploaded." )
+            s3.close()
+        } catch (e: S3Exception) {
+            System.err.println(e.message)
+            System.exit(1)
+        }
+    }
+
+    fun ListDocumentsAWS() {
+        val awsCreds = AwsBasicCredentials.create(
+            Credentials.ACCESS_KEY_ID,
+            Credentials.SECRET_ACCESS_KEY
+        )
+        val region: Region = Region.US_EAST_1
+        val s3 = S3Client.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+            .region(region)
+            .build()
+        val bucketName = "donaciones"
+
+        listBucketObjects(s3, bucketName)
     }
 
 
-
-
-    fun DBloadFiles(pDonations:List<donationLink>){
+    fun DBloadFiles(pDonations: List<donationLink>) {
 //        var url = URL(Credentials.FILE_URL)
-         lateinit  var destination : File
+        lateinit var destination: File
         println("Downloading files from google...")
         for (donationLink in pDonations) {
             destination = File(Credentials.SAVE_DIR + donationLink.id + ".json")
-            FileUtils.copyURLToFile(URL(donationLink.link.toString()) , destination)
+            FileUtils.copyURLToFile(URL(donationLink.link.toString()), destination)
         }
 
     }
 
     @Throws(Exception::class)
-    fun BringFiles(pFileN : String) {
+    fun BringFiles(pFileN: String) {
         val fileURL = Credentials.FILE_URL
         val saveDir = Credentials.SAVE_DIR
         try {
@@ -60,62 +124,61 @@ object S3JavaSDKExample : Throwable() {
         }
 
     }
-
-    @Throws(Exception::class)
-    fun PopulateSimpleBucket() {
-        val awsCreds = BasicAWSCredentials(Credentials.ACCESS_KEY_ID, Credentials.SECRET_ACCESS_KEY)
-        val s3Client = AmazonS3Client(awsCreds)
-        val bucketName = "donaciones"
-        val fileName = Credentials.FILE_TO_COPY
-        val file = File( Credentials.PATH_FILE + fileName)
-        try {
-            val putRequest1 = PutObjectRequest(bucketName, fileName, file)
-            val response1: PutObjectResult = s3Client.putObject(putRequest1)
-            System.out.println(
-                "Uploaded object encryption status is " +
-                        response1.getSSEAlgorithm()
-            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-            println("It was not able to store an unencrypted file in this folder: " + e.message)
-            System.exit(1)
-        }
-    }
-
-    @Throws(Exception::class)
-    fun PopulateBucket(donations : List<donationLink>) {
-        val awsCreds = BasicAWSCredentials(Credentials.ACCESS_KEY_ID, Credentials.SECRET_ACCESS_KEY)
-        val s3Client = AmazonS3Client(awsCreds)
-        val bucketName = "donaciones"
-        var i : Int = 0
-
-        try {
-            var fileName = ""
-            lateinit var file : File
-            lateinit var putRequest : PutObjectRequest
-            lateinit var response: PutObjectResult
-            println("Uploading files to ES3...")
-            donations.forEach {
-                if (i < 5){
-                    fileName = it.id + ".json"
-                    file = File(Credentials.SAVE_DIR + fileName)
-                    putRequest = PutObjectRequest(bucketName, fileName, file)
-                    response = s3Client.putObject(putRequest)
-                    i += 1
-                 }
-            }
-
-
-            System.out.println("Files Uploaded!! ")
+    //@Throws(Exception::class)
+//    fun PopulateSimpleBucket() {
+//        val awsCreds = BasicAWSCredentials(Credentials.ACCESS_KEY_ID, Credentials.SECRET_ACCESS_KEY)
+//        val s3Client = AmazonS3Client(awsCreds)
+//        val bucketName = "donaciones"
+//        val fileName = Credentials.FILE_TO_COPY
+//        val file = File( Credentials.PATH_FILE + fileName)
+//        try {
+//            val putRequest1 = PutObjectRequest(bucketName, fileName, file)
+//            val response1: PutObjectResult = s3Client.putObject(putRequest1)
 //            System.out.println(
-//                "Files Uploaded!! Object encryption status is " +  response.getSSEAlgorithm()
+//                "Uploaded object encryption status is " +
+//                        response1.getSSEAlgorithm()
 //            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-            println("It was not able to store an unencrypted file in this folder: " + e.message)
-            System.exit(1)
-        }
-    }
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            println("It was not able to store an unencrypted file in this folder: " + e.message)
+//            System.exit(1)
+//        }
+//    }
+
+//    @Throws(Exception::class)
+//    fun PopulateBucket(donations : List<donationLink>) {
+//        val awsCreds = BasicAWSCredentials(Credentials.ACCESS_KEY_ID, Credentials.SECRET_ACCESS_KEY)
+//        val s3Client = AmazonS3Client(awsCreds)
+//        val bucketName = "donaciones"
+//        var i : Int = 0
+//
+//        try {
+//            var fileName = ""
+//            lateinit var file : File
+//            lateinit var putRequest : PutObjectRequest
+//            lateinit var response: PutObjectResult
+//            println("Uploading files to ES3...")
+//            donations.forEach {
+//                if (i < 5){
+//                    fileName = it.id + ".json"
+//                    file = File(Credentials.SAVE_DIR + fileName)
+//                    putRequest = PutObjectRequest(bucketName, fileName, file)
+//                    response = s3Client.putObject(putRequest)
+//                    i += 1
+//                 }
+//            }
+//
+//
+//            System.out.println("Files Uploaded!! ")
+////            System.out.println(
+////                "Files Uploaded!! Object encryption status is " +  response.getSSEAlgorithm()
+////            )
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            println("It was not able to store an unencrypted file in this folder: " + e.message)
+//            System.exit(1)
+//        }
+//    }
 
     object HttpDownloadUtility {
         private const val BUFFER_SIZE = 4096
@@ -132,7 +195,10 @@ object S3JavaSDKExample : Throwable() {
             // always check HTTP response code first
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 var fileName = ""
-                val disposition: String = if(httpConn.getHeaderField("Content-Disposition")==null) "No disposition" else httpConn.getHeaderField("Content-Disposition")
+                val disposition: String =
+                    if (httpConn.getHeaderField("Content-Disposition") == null) "No disposition" else httpConn.getHeaderField(
+                        "Content-Disposition"
+                    )
                 val contentType: String = httpConn.getContentType()
                 val contentLength: Int = httpConn.getContentLength()
                 if (disposition != "No disposition") {
@@ -179,10 +245,9 @@ object S3JavaSDKExample : Throwable() {
     }
 
 
-
 }
 
-data class donationLink(val id:String,val link: String){}
+data class donationLink(val id: String, val link: String) {}
 
 //    fun Prueba(){
 //        val url = URL(Credentials.FILE_URL)
@@ -193,3 +258,31 @@ data class donationLink(val id:String,val link: String){}
 //        // Copy bytes from the URL to the destination file.
 //        FileUtils.copyURLToFile(url, destination)
 //    }
+fun listBucketObjects(s3: S3Client, bucketName: String?) {
+    try {
+        val listObjects = ListObjectsRequest
+            .builder()
+            .bucket(bucketName)
+            .build()
+        val res = s3.listObjects(listObjects)
+        val objects = res.contents()
+        val iterVals: ListIterator<*> = objects.listIterator()
+        while (iterVals.hasNext()) {
+            val myValue = iterVals.next() as S3Object
+            print("The name of the key is ${myValue.key()} ")
+            print("""The object is ${calKb(myValue.size())} KBs """)
+            print("""The owner is ${myValue.owner()}""")
+        }
+    } catch (e: S3Exception) {
+        System.err.println(e.awsErrorDetails().errorMessage())
+        System.exit(1)
+    }
+}
+
+//convert bytes to kbs
+private fun calKb(`val`: Long): Long {
+    return `val` / 1024
+}
+
+
+
